cbb3f8d6c0ce526a1fdaa148ed7803e7af2a8689
Holly Tuhake <hollyjeanhansen@gmail.com>
2017-10-30 17:04:37 -0500

    got it working, simpsons for life

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,13 @@
 console.log('js sourced');
 var myApp = angular.module('myApp', []);
-myApp.controller('SimpsonsController', function () {
-    
+myApp.controller('SimpsonsController', function ($http) {
+    var sc = this;
+    getQuote();
+    function getQuote(){
+        console.log('in getQuote')
+        $http.get('https://thesimpsonsquoteapi.glitch.me/quotes').then(function(response){
+            console.log(response);
+            sc.message = response.data[0].quote + ' - ' + response.data[0].character;
+        });
+    }
 });
--- server/server.js
+++ server/server.js
@@ -13,3 +13,4 @@ app.use(express.static('server/public'));
 app.listen(port, function () {
     console.log('listening on port', port);
 });
+
