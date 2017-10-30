1f49aa560d8a97abe10371de35c5e3d6144a1067
Holly Tuhake <hollyjeanhansen@gmail.com>
2017-10-30 16:41:42 -0500

    server up, initial page skeleton

---
+++ server/public/scripts/client.js
@@ -0,0 +1,5 @@
+console.log('js sourced');
+var myApp = angular.module('myApp', []);
+myApp.controller('SimpsonsController', function () {
+    
+});
---
+++ server/server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var port = process.env.PORT || 5000;
+
+console.log('connected to server');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json()); 
+app.use(express.static('server/public'));
+
+
+app.listen(port, function () {
+    console.log('listening on port', port);
+});
